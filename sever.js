9fed7a0511b2c3c885446606ee099f54ffa43224
anixjoseph <115452748+anixjoseph@users.noreply.github.com>
2025-03-16 18:18:54 +1100

    Added server.js and updated package.json

--- sever.js
+++ sever.js
@@ -1,11 +1,21 @@
+// Import the Express framework
 const express = require('express');
+// Create an instance of the Express application
 const app = express();
+// Define the port number on which the server will listen
 const port = 3000;
-
+/**
+ * Route: GET /
+ * Description: Handles requests to the root URL ("/").
+ * Response: Sends an HTML response with a welcome message.
+ */
 app.get('/', (req, res) => {
     res.send('<h1>Welcome to Anixs Node.js Web Server</h1>');
 });
-
+/**
+ * Starts the server and listens on the specified port.
+ * Logs a message to the console when the server is running.
+ */
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
